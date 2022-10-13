bd1b193cce25df646980a172acc0e015b3513c57
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-13 13:54:37 -0400

    conditional nav styling & props

--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -1,27 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-    // navigation categories
-    const categories = [
-        {
-          name: "commercial",
-          description:
-            "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-          name: "landscape",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-      ];
 
-    //   nav event listener function
-      function categorySelected(category) {
-        console.log(category)
-      }
+function Nav(props) {
+  // deconstruct props object
+  const {
+      categories = [],
+      setCurrentCategory,
+      currentCategory,
+    } = props;
 
+    // update document.title to match category selection
+    useEffect(() => {
+      document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
+
+    // component jsx
     return (
         <header className="flex-row px-1">
         <h2>
@@ -40,8 +34,8 @@ function Nav() {
               <span>Contact</span>
             </li>
             {categories.map(category => (
-                <li className="mx-1" key={category.name}>
-                    <span onClick={() => categorySelected(category.name)}>{capitalizeFirstLetter(category.name)}</span>
+                <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+                    <span onClick={() => setCurrentCategory(category)}>{capitalizeFirstLetter(category.name)}</span>
                 </li>
             ))}
                     

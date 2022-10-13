abe00f4a7c30a36a844fd502615f3419689c0a19
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-13 13:21:01 -0400

    gallery component w/ image rendering

--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav() {
     // navigation categories
@@ -22,7 +23,7 @@ function Nav() {
       }
 
     return (
-        <header>
+        <header className="flex-row px-1">
         <h2>
           <a data-testid="link" href="/">
             <span role="img" aria-label="camera"> 📸</span> Oh Snap!
@@ -35,12 +36,12 @@ function Nav() {
                 About me
               </a>
             </li>
-            <li>
+            <li className={"mx-2"}>
               <span>Contact</span>
             </li>
             {categories.map(category => (
                 <li className="mx-1" key={category.name}>
-                    <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+                    <span onClick={() => categorySelected(category.name)}>{capitalizeFirstLetter(category.name)}</span>
                 </li>
             ))}
                     
